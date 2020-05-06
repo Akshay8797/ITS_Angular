ad76e347651566eacc7768053a0bbb38bec46f46
Akshay8797 <60914748+Akshay8797@users.noreply.github.com>
2020-05-07 01:11:20 +0530

    /hr and /user/resetpassword

--- its-project/src/app/app.module.ts
+++ its-project/src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
-import {RegisterComponent} from './components/registration.component';
+import { RegisterComponent } from './components/registration.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './components/admin/admin.component';
-import { GetFreeTechPanelService } from 'src/app/services/get-free-panel.service'
-import { FormsModule } from '@angular/forms'; 
+import { HrService } from './components/hrCandidates.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ResetPassCompomnent } from './components/userResetPass.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-   AdminComponent,
-    RegisterComponent
+    AdminComponent,
+    RegisterComponent,
+    HrService,
+    ResetPassCompomnent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [GetFreeTechPanelService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
