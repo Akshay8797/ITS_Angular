f133e8e092a0c9eb4758ec438096d025adb3bf0a
Akshay8797 <60914748+Akshay8797@users.noreply.github.com>
2020-05-07 01:01:45 +0530

    /hr and /user/resetpaswword

---
+++ its-project/src/app/services/hrCandidates.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HrCandidateService { 
+    constructor(private _httpClient: HttpClient) {
+    }
+
+    getHrCandidates(): Observable<Object> {
+        let httpHeaders = new HttpHeaders()
+        .set('allow-origin-access-control', '*')
+        .set('Content-type', 'application/json');
+        
+        return this._httpClient.get('http://localhost:8080/hr', {headers: httpHeaders});
+    }
+}
