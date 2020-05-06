05e74a32d0ab5e38e8eb0f40c2b0df3f96a52a70
Akshay8797 <60914748+Akshay8797@users.noreply.github.com>
2020-05-07 01:03:47 +0530

    /hr and /user/resetpassword

--- its-project/src/app/app-routing.module.ts
+++ its-project/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HrService } from './components/hrCandidates.component';
+import { ResetPassCompomnent } from './components/userResetPass.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"hr", component:HrService},
+  {path:"user/resetpassword", component:ResetPassCompomnent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
