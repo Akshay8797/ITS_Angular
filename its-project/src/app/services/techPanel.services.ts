68edc1abc7890eff57b0923e5abb4cdf47459546
anishkumar321 <60914890+anishkumar321@users.noreply.github.com>
2020-05-08 02:07:00 +0530

    Update techPanel.services.ts

--- its-project/src/app/services/techPanel.services.ts
+++ its-project/src/app/services/techPanel.services.ts
@@ -11,20 +11,20 @@ export class TechPanelService{
         .set('allow-origin-access-control','*')
         .set('Content-type','application/json');
 
-    return this._httpClient.get('http://localhost:8083/tech',{headers: httpHeaders});
+    return this._httpClient.get('http://localhost:8080/tech',{headers: httpHeaders});
     }
     giveTechRating(interviewId,interviewScheduleObj):Observable<Object>{
         let httpHeaders=new HttpHeaders()
         .set('allow-origin-access-control','*')
         .set('Content-type','application/json');
 
-        return this._httpClient.post(`http://localhost:8083/tech/${interviewId}`, JSON.stringify(interviewScheduleObj), {headers: httpHeaders});
+        return this._httpClient.post(`http://localhost:8080/tech/${interviewId}`, JSON.stringify(interviewScheduleObj), {headers: httpHeaders});
     }
     getResults():Observable<Object>{
         let httpHeaders=new HttpHeaders()
         .set('allow-origin-access-control','*')
         .set('Content-type','application/json');
 
-    return this._httpClient.get('http://localhost:8083/tech/result',{headers: httpHeaders});
+    return this._httpClient.get('http://localhost:8080/tech/result',{headers: httpHeaders});
     }
 }
